16cf69da3ce348f52a490e88accce2f6618e67b4
Jēkabs Konošonoks <s22konojeka@vea.lv>
2024-02-28 09:50:18 +0200

    New: added array class

--- src/datastr/MyArrayList.java
+++ src/datastr/MyArrayList.java
@@ -1,5 +1,35 @@
 package datastr;
 
 public class MyArrayList {
+	private int[] list;
+	private final int LIST_DEFAULT_SIZE = 10;
+	private int size = LIST_DEFAULT_SIZE;
+	private int counter = 0;
 
+	
+	//Constructors
+	public MyArrayList() {
+		list = new int[size];
+	}
+	public MyArrayList(int inputSize) {
+		if(inputSize > 0) {
+			size = inputSize;
+		}
+		list = new int[size];
+	}
+	
+	//isEmpty
+	public boolean isEmpty() {
+		return counter == 0;
+	}
+	
+	//isFull
+	public boolean isFull() {
+		return counter == size;
+	}
+	
+	//howManyElements
+		public int howManyElements() {
+			return counter;
+		}
 }
